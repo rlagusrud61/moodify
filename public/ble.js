0bee956f96f648ff7cf5bac4b0e94f78d79be568
Puru Vaish <puruvaish24@outlook.com>
2020-10-16 05:09:05 +0200

    ADD: ble.js. this should complete the bridge.

---
+++ public/ble.js
@@ -0,0 +1,101 @@
+let filters = [];
+let myDevice;
+let myServer;
+let myService;
+let myCharacteristic;
+let myServiceUUID = '953f08c4-8c8f-46f4-a48b-07c18dfb3447';
+let flag = '000';
+let myCharacteristicUUID = '58590b45-241a-4230-b020-700ac827a8fb';
+let uint8array = new TextEncoder()
+let string = new TextDecoder()
+
+function getDevice() {
+    let services = [myServiceUUID];
+    if (services) {
+        filters.push({services: services});
+    }
+
+    let filterName = 'Moodify';
+    if (filterName) {
+        filters.push({name: filterName});
+    }
+
+    let options = {};
+    options.filters = filters;
+
+    console.log('Requesting Bluetooth Device...');
+    console.log('with ' + JSON.stringify(options));
+    navigator.bluetooth.requestDevice(options)
+        .then(device => {
+            console.log('> Name:             ' + device.name);
+            console.log('> Id:               ' + device.id);
+            console.log('> Connected:        ' + device.gatt.connected);
+        })
+        .then(function(device) {
+            // save the device returned so you can disconnect later:
+            myDevice = device;
+            console.log(device);
+            // connect to the device once you find it:
+            return device.gatt.connect();
+        })
+        .then(function(server) {
+            myServer = server;
+            // get the primary service:
+            return server.getPrimaryService(myServiceUUID);
+        })
+        .then(function(service) {
+            myService = service
+            // get the  characteristic:
+            return service.getCharacteristics();
+        })
+        .then(function(characteristics) {
+            // subscribe to the characteristic:
+            for (let c in characteristics) {
+                if (characteristics[c].uuid === myCharacteristic) {
+                    myCharacteristic = characteristics[c];
+                    myCharacteristic.startNotifications()
+                        .then(subscribeToChanges)
+                }
+            }
+        })
+        .catch(error => {
+            console.log('Argh! ' + error);
+        });
+}
+
+// subscribe to changes from the meter:
+function subscribeToChanges(characteristic) {
+    characteristic.oncharacteristicvaluechanged = handleData;
+}
+
+function handleData(event) {
+    // get the data buffer from the meter:
+    const buf = new Uint8Array(event.target.value);
+    console.log(string.decode(buf));
+}
+
+function getCurrentValue() {
+    myCharacteristic.readValue()
+        .then(value => {
+            console.log('Current value is: ' + value);
+            flag = string.decode(value);
+        })
+        .catch(error => {
+            console.log('Error: ', error);
+        })
+}
+
+function writeVal(newFlag) {
+        let commandValue = new Uint8Array(uint8array.encode(newFlag));
+        myCharacteristic.writeValue(commandValue)
+        .catch(error => {
+            console.log(error);
+        })
+}
+
+function disconnect() {
+    if (myDevice) {
+        // disconnect:
+        myDevice.gatt.disconnect();
+    }
+}
--- public/index.js
+++ public/index.js
@@ -1,84 +1,60 @@
-const manualLightModeUUID = '953f08c4-8c8f-46f4-a48b-07c18dfb3447';
-const moodLightModeServiceUUID = 'a9a23424-73cc-4140-80ea-b2eecc335760';
-const changeLightIntensityChar = '9de2dcc7-f9e4-4372-a559-05c41e37914e';
-const alarmSettingChar = 'bb2d7591-b7e4-4fc3-ae11-66a1cb637153';
-const changeColourChar = '92915619-d075-499e-a072-0da6f7325d37';
+let colours;
+let slidr;
+let opt1;
+let opt2;
+let disconModal = document.getElementById("disconModal");
 
-function onButtonClick() {
-    // let filters = [];
-    //
-    // let filterService = document.querySelector('#service').value;
-    // if (filterService.startsWith('0x')) {
-    //     filterService = parseInt(filterService);
-    // }
-    // if (filterService) {
-    //     filters.push({services: [filterService]});
-    // }
-    //
-    // let filterName = document.querySelector('#name').value;
-    // if (filterName) {
-    //     filters.push({name: filterName});
-    // }
-    //
-    // let filterNamePrefix = document.querySelector('#namePrefix').value;
-    // if (filterNamePrefix) {
-    //     filters.push({namePrefix: filterNamePrefix});
-    // }
-
-    let options = {};
-    options.acceptAllDevices = true;
-
-    console.log('Requesting Bluetooth Device...');
-    console.log('with ' + JSON.stringify(options));
-    navigator.bluetooth.requestDevice(options)
-        .then(device => {
-            console.log('> Name:             ' + device.name);
-            console.log('> Id:               ' + device.id);
-            console.log('> Connected:        ' + device.gatt.connected);
-        })
-        .catch(error => {
-            console.log('Argh! ' + error);
-        });
+function loadIn(){
+    colours = document.getElementById("colours");
+    slidr = document.getElementById("slidr");
+    opt1 = document.getElementById("contactChoice1");
+    opt2 = document.getElementById("contactChoice2");
 }
 
-// let device, alarmChar, lightInstensityChar, colourChooser;
-// connectButton.onclick = async () => {
-//     device = await navigator.bluetooth
-//         .requestDevice({
-//             filters: [{
-//                 services: [manualLightModeUUID, moodLightModeServiceUUID]
-//             }]
-//         });
-//     const server = await device.gatt.connect();
-//     const moodLightModeService =
-//         await server.getPrimaryService(manualLightModeUUID);
-//     const manualLightService =
-//         await server.getPrimaryService(manualLightModeUUID)
-//
-//     lightInstensityChar =
-//         await moodLightModeService.getCharacteristic(changeLightIntensityChar);
-//     alarmChar =
-//         await moodLightModeService.getCharacteristic(alarmSettingChar);
-//     colourChooser =
-//         await manualLightService.getCharacteristic(changeColourChar);
-//
-//     device.ongattserverdisconnected = disconnect;
-//     onnected.style.display = 'block';
-//     connectButton.style.display = 'none';
-//     disconnectButton.style.display = 'initial';
-// };
-//
-// const disconnect = () => {
-//     device = null;
-//     lightInstensityChar = null;
-//     alarmChar = null;
-//     colourChooser = null;
-//     connected.style.display = 'none';
-//     connectButton.style.display = 'initial';
-//     disconnectButton.style.display = 'none';
-// };
-//
-// disconnectButton.onclick = async () => {
-//     await device.gatt.disconnect();
-//     disconnect();
-// };
+
+function updateChoice(){
+    colours.style.visibility="hidden";
+    slidr.style.visibility="hidden";
+    
+    if(opt1.checked===true){colours.style.visibility="visible"}
+    if(opt2.checked===true){slidr.style.visibility="visible"}
+    /*else{}*/
+}
+
+
+
+let span = document.getElementsByClassName("close")[0];
+
+function modalPopUp() {disconModal.style.display = "block";}
+
+function closing(){disconModal.style.display = "none";}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target === disconModal) {
+    modal.style.display = "none";
+  }
+} 
+
+// Debugger function for Master Puru <3
+function sendUpdate(pressed) {
+  let manualLight = document.getElementById("ManualLight");
+  let autoLDR = document.getElementById("LightIntensityMode");
+  let text;
+  if (manualLight.checked === true && pressed === 0) {
+      autoLDR.checked = false;
+      text = "110"
+
+  } else if (autoLDR.checked === true) {
+      manualLight.checked = false;
+      text = "001"
+  } else if (autoLDR.checked === false && manualLight.checked === false) {
+      text = "000"
+  }
+  writeVal(text)
+  console.log(text)
+}
+
+function disconnect(){
+  alert("You are now disconnected. Bai!");
+}
